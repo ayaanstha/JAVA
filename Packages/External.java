c7f56f5203fa99e2b2598b1cccb3903902092d4e
ayaanstha <ayaanshrestha.cs23@bmsce.ac.in>
2024-11-20 10:10:21 +0530

    External.java

--- Packages/External.java
+++ Packages/External.java
@@ -1,17 +1,17 @@
 package SEE;
 
-import CIE.Student;  // Importing Student class from the CIE package
+import CIE.Student;  
 
 public class External extends Student {
     public int[] externalMarks;
 
-    // Constructor to initialize student details and external marks
+    
     public External(String usn, String name, int sem, int[] externalMarks) {
         super(usn, name, sem);
         this.externalMarks = externalMarks;
     }
 
-    // Method to print external marks
+    
     public void printExternalMarks() {
         System.out.println("External Marks for " + name + " (" + usn + "):");
         for (int i = 0; i < externalMarks.length; i++) {
