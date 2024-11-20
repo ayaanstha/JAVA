4a1e6d5f0005882e6c872a2f537bb09349e680a4
ayaanstha <ayaanshrestha.cs23@bmsce.ac.in>
2024-11-20 10:10:49 +0530

    Internal.java

--- Packages/Internal.java
+++ Packages/Internal.java
@@ -3,13 +3,13 @@ package CIE;
 public class Internals extends Student {
     public int[] internalMarks;
 
-    // Constructor to initialize student details and internal marks
+    
     public Internals(String usn, String name, int sem, int[] internalMarks) {
         super(usn, name, sem);
         this.internalMarks = internalMarks;
     }
 
-    // Method to print internal marks
+   
     public void printInternalMarks() {
         System.out.println("Internal Marks for " + name + " (" + usn + "):");
         for (int i = 0; i < internalMarks.length; i++) {
